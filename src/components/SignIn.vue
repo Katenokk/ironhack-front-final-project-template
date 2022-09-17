9c8fc43d19d4d073e25e42da19d048354cc91742
Katia Obraztsova <katia.obr@gmail.com>
2022-09-17 20:55:39 +0200

    alert en el signIn

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -38,6 +38,16 @@
         </div>
 
       </div>
+      <div class="alert alert-success shadow-lg">
+  <div>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg>
+
+    <span>Un agradecimiento especial a Antonio e Ibone por haberme dado una idea tan chula</span>
+  </div>
+</div>
       <form
         class="flex flex-col mt-6 space-y-6"
         @submit.prevent="signIn(email, password)"
